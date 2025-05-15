1df3a44ab73585db02d9a5cbab538ad1a1bd218a
kss418 <108602943+kss418@users.noreply.github.com>
2025-05-15 22:55:18 +0900

    Add files via upload

---
+++ B_문자열 줄이기/polygon/validator.cpp
@@ -0,0 +1,23 @@
+#include "testlib.h"
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long; using pll = pair<ll, ll>;
+#define fastio cin.tie(0), cout.tie(0), ios::sync_with_stdio(0);
+constexpr ll MAX = 50000;
+
+int main(int argc, char* argv[]) {
+    fastio;
+    registerValidation(argc, argv);
+
+    ll tc = inf.readLong(1ll, 1000ll, "tc");
+    inf.readEoln();
+
+    while(tc--){
+        string s = inf.readWord("[a-z]+", "s");
+        inf.readEoln();
+    }
+    inf.readEof();
+
+
+    return 0;
+}
